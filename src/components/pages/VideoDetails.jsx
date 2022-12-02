2adc91c340d7ab4d2c0572e20b9b7e11b0d400ef
mj0614k <mj0614k@gmail.com>
2022-12-02 17:18:42 +0900

    221202

--- src/components/pages/VideoDetails.jsx
+++ src/components/pages/VideoDetails.jsx
@@ -1,61 +1,74 @@
 import React, { useEffect, useState } from "react";
-import ReactPlayer from 'react-player'
+import ReactPlayer from "react-player";
 import { useParams } from "react-router-dom";
 import Header from "../layout/Header";
 // import Loader from "../layout/Loader";
 
 const VideoDetails = () => {
-    const [video, setVideo] = useState([]);
-    const { videoId } = useParams();
-    // const [random, setRandom] = useState([]);
+  const [video, setVideo] = useState([]);
+  const { videoId } = useParams();
+  // const [random, setRandom] = useState([]);
 
   useEffect(() => {
-    fetch(
-      `https://images-api.nasa.gov/search?media_type=video&nasa_id=${videoId}`
-    )
+    fetch(`https://images-api.nasa.gov/search?media_type=video&nasa_id=${videoId}`)
       .then((response) => response.json())
       .then((result) => setVideo(result.collection))
       .catch((error) => console.log("error", error));
   }, []);
 
-//   useEffect(() => {
-//     fetch(
-//       `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=4`
-//     )
-//     .then((response) => response.json())
-//       .then((result) => setRandom(result))
-//       .catch((error) => console.log("error", error));
-//   })
-  console.log(video)
+  //   useEffect(() => {
+  //     fetch(
+  //       `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&count=4`
+  //     )
+  //     .then((response) => response.json())
+  //       .then((result) => setRandom(result))
+  //       .catch((error) => console.log("error", error));
+  //   })
+  // console.log(video);
   // if (!image?.length) return <Loader />;
-  
+
+  //images-assets.nasa.gov/video/KSC-20190828-VP-MMS01-0001-Tom_Joyner_Explores_Moon_to_Mars_Master-3229627/KSC-20190828-VP-MMS01-0001-Tom_Joyner_Explores_Moon_to_Mars_Master-3229627~orig.mp4
+  //images-assets.nasa.gov/video/KSC-20190828-VP-MMS01-0001-Tom_Joyner_Explores_Moon_to_Mars_Master-3229627_Tom Joyner Explores Moon to Mars/KSC-20190828-VP-MMS01-0001-Tom_Joyner_Explores_Moon_to_Mars_Master-3229627_Tom Joyner Explores Moon to Mars~orig.mp4
   return (
     <>
       <Header />
       <main className="container">
         <section className="section detail">
-            <div className="detail__left">
+          <div className="detail__left">
             <figure className="main__img">
-                <ReactPlayer url={video?.href} controls />
+              <ReactPlayer url={`http://images-assets.nasa.gov/video/${video?.items?.[0].data?.[0].nasa_id}/${video?.items?.[0].data?.[0].nasa_id}~orig.mp4`} controls />
             </figure>
             <div className="sub__img">
-                {/* <a href="#"><div style={{backgroundImage: `url(${random?.[0]?.url})`}}></div></a> */}
-                <a href="#"><div></div></a>
-                <a href="#"><div></div></a>
-                <a href="#"><div></div></a>
+              {/* <a href="#"><div style={{backgroundImage: `url(${random?.[0]?.url})`}}></div></a> */}
+              <a href="#">
+                <div></div>
+              </a>
+              <a href="#">
+                <div></div>
+              </a>
+              <a href="#">
+                <div></div>
+              </a>
             </div>
-            </div>
-            <div className="detail__right">
+          </div>
+          <div className="detail__right">
             <h2 className="detail__title">{video?.items?.[0].data?.[0]?.title}</h2>
-            <p className="detail__desc">
-                {video?.items?.[0].data?.[0]?.description}
-            </p>
+            <p className="detail__desc">{video?.items?.[0].data?.[0]?.description}</p>
             <div className="detail__info">
-                <li><span>ID : </span><span>{video?.items?.[0].data?.[0].nasa_id}</span></li>
-                <li><span>TYPE : </span><span>{video?.items?.[0].data?.[0].media_type}</span></li>
-                <li><span>DATE : </span><span>{video?.items?.[0].data?.[0].date_created}</span></li>
+              <li>
+                <span>ID : </span>
+                <span>{video?.items?.[0].data?.[0].nasa_id}</span>
+              </li>
+              <li>
+                <span>TYPE : </span>
+                <span>{video?.items?.[0].data?.[0].media_type}</span>
+              </li>
+              <li>
+                <span>DATE : </span>
+                <span>{video?.items?.[0].data?.[0].date_created}</span>
+              </li>
             </div>
-            </div>
+          </div>
         </section>
       </main>
     </>
--- src/utils/fetchAPI.js
+++ src/utils/fetchAPI.js
@@ -1,20 +1,20 @@
-import axios from 'axios'
+import axios from "axios";
 
-const BASE_URL = 'https://api.nasa.gov'
+const BASE_URL = "https://api.nasa.gov";
 
 const options = {
   url: BASE_URL,
   params: {
-    maxResults: '50',
+    maxResults: "5",
   },
   headers: {
-    'api_key': process.env.REACT_APP_NASA_API_KEY,
+    api_key: process.env.REACT_APP_NASA_API_KEY,
     // 'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
   },
-}
+};
 
 export const fetchAPI = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options)
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-  return data
-}
+  return data;
+};
